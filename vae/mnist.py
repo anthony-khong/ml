35edaa8abe34bf066d77784e30305823bee0c4a6
akkhong <anthony.kusumo.khong@gmail.com>
2017-05-08 15:56:11 +0700

    First training instance of VAE

--- vae/mnist.py
+++ vae/mnist.py
@@ -1,10 +1,11 @@
 import numpy as np
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
+from tensorflow.contrib import distributions as dd
 
 import reprod as rr
 
-MNIST_PATH = rr.reprod.dropbox() + '/datasets/mnist/'
+MNIST_PATH = rr.paths.dropbox() + '/datasets/mnist/'
 
 def load_mnist(n_train=1000, n_test=1000):
     sample_ix = lambda n: np.random.choice(np.arange(n), size=n, replace=False)
@@ -14,11 +15,10 @@ def load_mnist(n_train=1000, n_test=1000):
             'test': mnist.test.images[test_ix]}
     return data
 
-
 if __name__ == '__main__':
     data = load_mnist()
 
-    n_feats, n_hiddens, n_latents = 784, 100, 2
+    n_feats, n_hiddens, n_latents = 784, 100, 10
     make_weight = lambda shape: tf.Variable(tf.truncated_normal(shape, stddev=0.1))
     make_bias = lambda n: tf.Variable(tf.constant(0.1, shape=[n]))
 
@@ -46,10 +46,23 @@ if __name__ == '__main__':
     hidden_ = tf.nn.relu6(tf.matmul(latents, weights_[0]) + biases_[0])
     preds = tf.sigmoid(tf.matmul(hidden_, weights_[1]) + biases_[1])
 
+    # Objective function
+    log_likelihood = feats*tf.log(preds) + (1.0-feats)*tf.log(1.0-preds)
+    prior_divergence = (
+            dd.Normal(mean, std).log_prob(latents)
+            - dd.Normal(0.0, 1.0).log_prob(latents)
+            )
+    aggregate = lambda x: tf.reduce_mean(tf.reduce_sum(x, 1))
+    objective = aggregate(prior_divergence) - aggregate(log_likelihood)
+
+    # Optimisation
+    train_step = tf.train.AdamOptimizer(1e-3).minimize(objective)
     sess = tf.Session()
     sess.run(tf.global_variables_initializer())
-    x = data['train'][:10]
-    sess.run(preds, feed_dict={feats: x})
+    for _ in range(10000):
+        ix = np.random.choice(np.arange(1000), size=16, replace=False)
+        x, x_test = data['train'][ix], data['test'][ix]
+        print(sess.run(objective, feed_dict={feats: x_test}))
+        sess.run(train_step, feed_dict={feats: x})
 
-
-             
+    # Successive image generations
